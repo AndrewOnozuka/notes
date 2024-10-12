a45d719da5b3e37c2e38b60350f087bcaead7b85
Ryo Andrew Onozuka <ryoandrewonozuka@gmail.com>
2024-10-11 20:41:34 -0700

    updates

--- ucsd/ece111/HW/HW1/Lab1/decoder_2to4_behavioral.sv
+++ ucsd/ece111/HW/HW1/Lab1/decoder_2to4_behavioral.sv
@@ -3,13 +3,14 @@ module decoder(
  input  logic[1:0] sel,
  output logic[3:0] out
 );
- always @(sel or out)
+ always @(sel)
   begin
    case (sel)
-    2'b00  : out = 4'b0001;
-
-    // Student to add remainder of the code   
-
+    2'b00  : out = 4'b0001; // Output for sel = 00
+    2'b01  : out = 4'b0010; // Output for sel = 01
+    2'b10  : out = 4'b0100; // Output for sel = 10
+    2'b11  : out = 4'b1000; // Output for sel = 11
+    default: out = 4'b0000; // Default case 
    endcase
   end
 endmodule
--- ucsd/ece111/HW/HW1/Lab1/decoder_2to4_dataflow.sv
+++ ucsd/ece111/HW/HW1/Lab1/decoder_2to4_dataflow.sv
@@ -3,7 +3,9 @@ module decoder(
  input  logic[1:0] sel,
  output logic[3:0] out
 );
- assign out[0] = (!sel[0]) && (!sel[1]);
- // Student to add remainder of the assign statements
+ assign out[0] = (!sel[0]) && (!sel[1]); // When sel = 00, out[0] is 1
+ assign out[1] = (sel[0]) && (!sel[1]);  // When sel = 01, out[1] is 1
+ assign out[2] = (!sel[0]) && (sel[1]);  // When sel = 10, out[2] is 1
+ assign out[3] = (sel[0]) && (sel[1]);   // When sel = 11,
 
 endmodule
