9ebb3c40dcce5fd7f42e191a7ed4907ae4e69794
Ryo Andrew Onozuka <ryoandrewonozuka@gmail.com>
2025-01-06 17:11:00 -0800

    cse167

---
+++ ucsd/cse167/hw/hw0-windows/src/HelloGL.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+// Use of degrees is deprecated. Use radians for GLM functions
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <FreeImage.h>
+
+static const int width = 400;
+static const int height = 300;
+
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+    glFlush();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 27: // Escape to quit
+        exit(0);
+        break;
+    }
+}
+
+int main(int argc, char** argv) {
+    // BEGIN CREATE WINDOW
+    glutInit(&argc, argv);
+
+    glutInitContextVersion(3, 1);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("Hello Window");
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+    }
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    // END CREATE WINDOW
+
+    std::cout << "Press ESC to quit." << std::endl;
+    glClearColor(0.0, 0.2, 0.5, 1.0);
+    glViewport(0, 0, width, height);
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+}
